fb59ef29a1205a63daff6ccd7e19d8108ea76236
Manindra_Tamang <manindra.tamang@festnepal.com>
2022-07-12 14:35:05 +0545

    testing

---
+++ app/Http/Controllers/BotManController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use BotMan\BotMan\BotMan;
+use BotMan\BotMan\Messages\Incoming\Answer;
+
+
+class BotManController extends Controller
+{
+    public function handle(){
+
+    }
+}
